4588df49419072b8f6b0116fe1ed64ec97be2dc4
hs.hernandez <hs.hernandez@ISIS2603S3-0024.sis.virtual.uniandes.edu.co>
2017-11-14 14:54:07 -0500

    error de impresión cock

--- farmacia-web/src/main/webapp/src/modules/producto/Producto.ctrl.js
+++ farmacia-web/src/main/webapp/src/modules/producto/Producto.ctrl.js
@@ -25,7 +25,6 @@
 
             this.createAndUpdate = function (id) {
 
-                console.log(id);
                 currentRecord = $scope.currentRecord;
 
                     if (id == null || id == undefined) {
